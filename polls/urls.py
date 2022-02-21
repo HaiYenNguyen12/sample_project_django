b0db401f7bc07f02027b35435eb8fa63900a5d41
Nguyen Thi Hai Yen <nguyenthihaiyen120397@gmail.com>
2022-02-21 23:41:15 +0700

    Create View and Path in App

--- polls/urls.py
+++ polls/urls.py
@@ -23,6 +23,19 @@ from django.urls import path
 
 from . import views
 
+
+
+app_name = 'polls' 
+# Them app_name de phan biet url voi cac app khac
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+    # /polls/5/
+    path('<int:question_id>/',views.detail, name='detail'),
+    # /polls/5/results/
+    path('<int:question_id>/resutls/',views.results, name='results'),
+    # /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    
 ]
