dbe2d428f3a0141fe269545f21853cfdeae1042f
Nguyen Thi Hai Yen <nguyenthihaiyen120397@gmail.com>
2022-02-23 21:07:03 +0700

    Part 5 : Form and Generic View

--- polls/urls.py
+++ polls/urls.py
@@ -28,13 +28,9 @@ from . import views
 app_name = 'polls' 
 # Them app_name de phan biet url voi cac app khac
 urlpatterns = [
-    # /polls/
-    path('', views.index, name='index'),
-    # /polls/5/
-    path('<int:question_id>/',views.detail, name='detail'),
-    # /polls/5/results/
-    path('<int:question_id>/resutls/',views.results, name='results'),
-    # /polls/5/vote
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
     
     
